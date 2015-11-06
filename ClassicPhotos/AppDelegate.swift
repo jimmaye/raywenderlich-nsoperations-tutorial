8103dd9a2a73a12678d40f89a33e8e210c4c7de6
Jimmie Jensen <jimmie.jensen@jayway.com>
2015-11-06 11:49:42 +0100

    project work

--- ClassicPhotos/AppDelegate.swift
+++ ClassicPhotos/AppDelegate.swift
@@ -10,9 +10,6 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-                            
-    var window: UIWindow?
-
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         return true
@@ -39,7 +36,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
