bbedb0a5634dca21e6cbc187a0a06e1dce80c826
Jimmie Jensen <jimmie.jensen@jayway.com>
2015-11-06 14:27:22 +0100

    added implementations to photo operations

--- ClassicPhotos/PhotoOperations.swift
+++ ClassicPhotos/PhotoOperations.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2015 raywenderlich. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+// This enum contains all the possible states a photo record can be in
+enum PhotoRecordState {
+    case New, Downloaded, Filtered, Failed
+}
+
+class PhotoRecord {
+    let name:String
+    let url:NSURL
+    var state = PhotoRecordState.New
+    var image = UIImage(named: "Placeholder")
+    
+    init(name:String, url:NSURL) {
+        self.name = name
+        self.url = url
+    }
+}
+
+class PendingOperations {
+    lazy var downloadsInProgress = [NSIndexPath:NSOperation]()
+    lazy var downloadQueue:NSOperationQueue = {
+        var queue = NSOperationQueue()
+        queue.name = "Download queue"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+    
+    lazy var filtrationsInProgress = [NSIndexPath:NSOperation]()
+    lazy var filtrationQueue:NSOperationQueue = {
+        var queue = NSOperationQueue()
+        queue.name = "Image Filtration queue"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+}
